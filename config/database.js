e26f5b1d93abd225300e46e07a4664b6835ea018
mtorreblanca@liricus.com.ar <mtorreblanca@liricus.com.ar>
2019-02-09 19:03:14 -0300

    process_env

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
     remoteUrl : 'mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu',
-    localUrl: 'mongodb://admin_dummy:dummy_pwd@ec2-18-206-40-90.compute-1.amazonaws.com:27017/dummyDB'
+    localUrl: process.env.MONGO_URL
 };
